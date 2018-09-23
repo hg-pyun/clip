fef7f3e0db5abe1f711b669957d23b20571fd6a4
haegul <phg2491@gmail.com>
2018-09-23 23:50:29 +0900

    Add react library (#1)

--- configs/webpack.config.common.js
+++ configs/webpack.config.common.js
@@ -2,16 +2,17 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, '../src/js/index.ts'),
+    entry: path.resolve(__dirname, '../src/js/index.tsx'),
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.ts?x$/,
+                resolve: { extensions: ['.ts', '.tsx']},
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                        presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
                         plugins: ['@babel/plugin-proposal-object-rest-spread']
                     }
                 }
---
+++ src/js/containers/App.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Rapid Installer!</h1>
+            </div>
+        )
+    }
+
+}
+
+export default App;
+
